505cf463cafc22ffd4666deb782d1fb4d7c49c64
이민호 <minho.lee0716@gmail.com>
2021-07-29 13:40:26 +0900

    Variable(let, var)

--- main.js
+++ main.js
@@ -3,7 +3,33 @@
 // use this for Vanilla Javascript.
 'use strict';
 
-console.log('hello world');
+// console.log('hello world');
 
-let a;
-a = 6;
+// let a;
+// a = 6;
+
+// 2. Variable
+// let (added in ES6)
+
+let globalName = 'global name';
+{
+  let name = 'ellie';
+  console.log(name);
+  name = 'hello';
+  console.log(name);
+	console.log(globalName);
+}
+
+console.log(name);
+console.log(globalName);
+
+// var (don't ever use this!)
+// var hoisting (move declaration from bottom to top)
+// has no block scope
+
+{
+	age = 4;
+	var age;
+}
+
+console.log(age);
