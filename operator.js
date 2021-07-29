bc5ca626d44a5452ba4e8326ac5d60c0505f6a2d
이민호 <minho.lee0716@gmail.com>
2021-07-29 16:22:39 +0900

    Increment Decrement

--- operator.js
+++ operator.js
@@ -15,3 +15,21 @@ console.log(1 / 1); // divide
 console.log(1 * 1); // multiply
 console.log(1 % 1); // remainder
 console.log(1 ** 1); // exponentiation
+
+// 3. Increment and decrement operators
+let counter = 2;
+const preIncrement = ++counter;
+// counter = counter + 1;
+// preIncrement = counter;
+console.log(`preIncrement: ${preIncrement}, counter: ${counter}`) ; // 3 3
+
+const postIncrement = counter++;
+// postIncrement = counter;
+// counter = counter + 1;
+console.log(`postIncrement: ${postIncrement}, counter: ${counter}`); // 3 4
+
+const preDecrement = --counter;
+console.log(`preDecrement: ${preDecrement}, counter: ${counter}`); // 3 3
+
+const postDecrement = counter--;
+console.log(`postDecrement: ${postDecrement}, counter: ${counter}`); // 3 2
