bf3892314fd0c747affccb95303fa876c02b800d
이민호 <minho.lee0716@gmail.com>
2021-07-30 11:53:38 +0900

    Getter & Setter

--- class.js
+++ class.js
@@ -25,3 +25,26 @@ const minho = new Person('minho', 24);
 console.log(minho.name);
 console.log(minho.age);
 minho.speak();
+
+// 2. Getter and setters
+class User {
+    constructor(firstName, lastName, age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    get age() {
+        return this._age;
+    }
+
+    set age(value) {
+        // if (value < 0) {
+        //     throw Error('age can not be negative');
+        // }
+        this._age = value < 0 ? 0 : value;
+    }
+}
+
+const user1 = new User('Steve', 'Job', -1);
+console.log(user1.age);
