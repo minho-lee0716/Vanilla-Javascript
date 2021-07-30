b812175474dc50cc3ddbb599904a9f333e29955a
이민호 <minho.lee0716@gmail.com>
2021-07-30 17:15:53 +0900

    Class Inheritance(상속, 오버라이딩 등)

--- class.js
+++ class.js
@@ -77,3 +77,41 @@ const a1 = new Article(1);
 const a2 = new Article(2);
 console.log(Article.publisher);
 Article.printPublisher();
+
+// 5. Inheritance
+// a way for one class to extend another class.
+
+class Shape {
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw() {
+        console.log(`drawing ${this.color} color!`);
+    }
+
+    getArea() {
+        return this.width * this.height;
+    }
+}
+
+class Rectangle extends Shape {}
+class Triangle extends Shape {
+    draw() {
+        super.draw();
+        console.log('🔺');
+    }
+    getArea() {
+        return (this.width * this.height) / 2;
+    }
+}
+
+const rectangle = new Rectangle(20, 20, 'blue');
+rectangle.draw();
+console.log(rectangle.getArea());
+
+const triangle = new Triangle(20, 20, 'red');
+triangle.draw();
+console.log(triangle.getArea());
