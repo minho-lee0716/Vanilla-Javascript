51a3e22c94189bad9176456caba80588d0a822cb
이민호 <minho.lee0716@gmail.com>
2021-07-30 11:44:55 +0900

    Class 선언 & Object 생성

---
+++ class.js
@@ -0,0 +1,27 @@
+'use strict';
+// Object-oriented programming
+// class: template
+// object: instance of a class
+// JavaScript classes
+// - introduced in ES6
+// - syntactical sugar over prototype-based inheritance
+
+// 1. Class declarations
+class Person {
+    // constructor
+    constructor(name, age) {
+        // fields
+        this.name = name;
+        this.age = age;
+    }
+
+    // methods
+    speak() {
+        console.log(`${this.name}: hello!`);
+    }
+}
+
+const minho = new Person('minho', 24);
+console.log(minho.name);
+console.log(minho.age);
+minho.speak();
