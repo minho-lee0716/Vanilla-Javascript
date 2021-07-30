c5103011d9cc6406a628bbd1b20d8767cfde1165
이민호 <minho.lee0716@gmail.com>
2021-07-30 10:25:13 +0900

    Function(Early Return & Early Exit) Logic

--- function.js
+++ function.js
@@ -71,3 +71,19 @@ function sum(a, b) {
 }
 const result = sum(1, 2); // 3
 console.log(`sum: ${sum(1, 2)}`);
+
+// 7. Early return, early exit
+// bad
+function upgradeUser(user) {
+    if (user.point > 10) {
+        // long upgrade logic...
+    }
+}
+
+// good
+function upgradeUser(user) {
+    if (user.point <= 10) {
+        return;
+    }
+    // long upgrade logic...
+}
