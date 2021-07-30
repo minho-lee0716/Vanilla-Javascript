8f5793696d34ead65ac8497dbcea3e496fb56b87
이민호 <minho.lee0716@gmail.com>
2021-07-30 09:54:06 +0900

    Add: function.js File & Learn Function of JS

---
+++ function.js
@@ -0,0 +1,22 @@
+// Function
+// - Fundamental building block in the program
+// - Subprogram can be used multiple times
+// - Performs a task or calculates a value
+
+// - 1. Function declaration
+// function name(param1, param2) { body... return; }
+// one function === one thing
+// nameing: doSomething, command, verb
+// e.g. createCardAndPoint -> createCard, createPoint
+// function is object in JS
+
+function printHello() {
+    console.log('hello');
+}
+printHello();
+
+function log(message) {
+    console.log(message);
+}
+log('Hello@');
+log(1234);
