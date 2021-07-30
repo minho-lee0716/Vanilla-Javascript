219b2a38a288bb5c5e1fd0b63c40b42c6f651cde
이민호 <minho.lee0716@gmail.com>
2021-07-30 11:35:42 +0900

    Practice calculate function

--- function.js
+++ function.js
@@ -11,12 +11,12 @@
 // function is object in JS
 
 function printHello() {
-    console.log('hello');
+	console.log('hello');
 }
 printHello();
 
 function log(message) {
-    console.log(message);
+	console.log(message);
 }
 log('Hello@');
 log(1234);
@@ -26,7 +26,7 @@ log(1234);
 // object parameters: passed by reference
 
 function changeName(obj) {
-    obj.name = 'coder';
+	obj.name = 'coder';
 }
 const ellie = { name: 'ellie' };
 // console.log(ellie);
@@ -35,39 +35,39 @@ console.log(ellie);
 
 // 3. Default parameters (added in ES6)
 function showMessage(message, from = 'unknown') {
-    // if (from === undefined) {
-    //     from = 'unknown'
-    // }
-    console.log(`${message} by ${from}`);
+	// if (from === undefined) {
+	// 	from = 'unknown'
+	// }
+	console.log(`${message} by ${from}`);
 }
 showMessage('Hi!');
 
 // 4. Rest parameters (added in ES6)
 function printAll(...args) {
-    // for (let i = 0; i < args.length; i++) {
-    //     console.log(args[i]);
-    // }
+	// for (let i = 0; i < args.length; i++) {
+	//   console.log(args[i]);
+	// }
 
-    // for (const arg of args) {
-    //     console.log(arg);
-    // }
+	// for (const arg of args) {
+	// 	console.log(arg);
+	// }
 
-    args.forEach((arg) => console.log(arg));
+	args.forEach((arg) => console.log(arg));
 }
 printAll('dream', 'coding', 'ellie');
 
 // 5. Local scope
 let globalMessage = 'global'; // global variable
 function printMessage() {
-    let message = 'hello';
-    console.log(message); // local variable
-    console.log(globalMessage);
+	let message = 'hello';
+	console.log(message); // local variable
+	console.log(globalMessage);
 }
 printMessage();
 
 // 6. Return a value
 function sum(a, b) {
-    return a + b;
+	return a + b;
 }
 const result = sum(1, 2); // 3
 console.log(`sum: ${sum(1, 2)}`);
@@ -75,17 +75,17 @@ console.log(`sum: ${sum(1, 2)}`);
 // 7. Early return, early exit
 // bad
 function upgradeUser(user) {
-    if (user.point > 10) {
-        // long upgrade logic...
-    }
+	if (user.point > 10) {
+		// long upgrade logic...
+	}
 }
 
 // good
 function upgradeUser(user) {
-    if (user.point <= 10) {
-        return;
-    }
-    // long upgrade logic...
+	if (user.point <= 10) {
+		return;
+	}
+	// long upgrade logic...
 }
 
 // First-class function
@@ -98,7 +98,7 @@ function upgradeUser(user) {
 // a function declaration can be called earlier than it is defined. (hoisted)
 // a function expression is created when the execution reaches it.
 const print = function () { // anonymous function
-    console.log('print');
+	console.log('print');
 }
 print();
 const printAgain = print;
@@ -108,23 +108,23 @@ console.log(sumAgain(1, 3));
 
 // 2. Callback function using function expression
 function randomQuiz(answer, printYes, printNo) {
-    if (answer === 'love you') {
-        printYes();
-    } else {
-        printNo();
-    }
+	if (answer === 'love you') {
+		printYes();
+	} else {
+		printNo();
+	}
 }
 
 // anonymous function
 const printYes = function() {
-    console.log('yes!');
+	console.log('yes!');
 }
 
 // named function
 // better debugging in debugger's stack traces
 // recursions
 const printNo = function print() {
-    console.log('no!');
+	console.log('no!');
 }
 randomQuiz('wrong', printYes, printNo);
 randomQuiz('love you', printYes, printNo);
@@ -132,18 +132,54 @@ randomQuiz('love you', printYes, printNo);
 // Arrow function
 // always anonymous
 const simplePrint = function () {
-    console.log('simplePrint!');
+	console.log('simplePrint!');
 }
 
 // Same this
 // const simplePrint = () => console.log('simplePrint!');
 const add = (a, b) => a + b;
 const simpleMultiply = (a, b) => {
-    // do something more
-    return a * b;
+	// do something more
+	return a * b;
 }
 
 // IIFE: Immediately Invoked Function Expression
 (function hello() {
-    console.log('IIFE');
+  console.log('IIFE');
 })();
+
+// function calculate(command, a, b)
+// command: add, substract, divide, multiply, remainder
+
+const calculate = (command, a, b) => {
+    switch (command) {
+        case 'add':
+            return a + b;
+            // break;
+
+        case 'substract':
+            return a / b;
+            // break;
+
+        case 'divide':
+            return a / b;
+            // break;
+
+        case 'remainder':
+            return a % b;
+            // break;
+
+        default:
+            return 'no command';
+            // break;
+    }
+}
+
+console.log(calculate('add', 1, 3));
+console.log(calculate('substract', 1, 3));
+console.log(calculate('divide', 5, 3));
+console.log(calculate('remainder', 5, 3));
+console.log(calculate('asdf', 1, 3));
+// console.log(calculate('add', 1, 3));
+// console.log(calculate('add', 1, 3));
+// console.log(calculate('add', 1, 3));
