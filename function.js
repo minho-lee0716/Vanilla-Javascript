dedb0a39a901987c9d474075d577366bc20783b1
이민호 <minho.lee0716@gmail.com>
2021-07-30 10:10:24 +0900

    Rest parameters(Like args in Python)

--- function.js
+++ function.js
@@ -41,3 +41,17 @@ function showMessage(message, from = 'unknown') {
     console.log(`${message} by ${from}`);
 }
 showMessage('Hi!');
+
+// 4. Rest parameters (added in ES6)
+function printAll(...args) {
+    // for (let i = 0; i < args.length; i++) {
+    //     console.log(args[i]);
+    // }
+
+    // for (const arg of args) {
+    //     console.log(arg);
+    // }
+
+    args.forEach((arg) => console.log(arg));
+}
+printAll('dream', 'coding', 'ellie');
